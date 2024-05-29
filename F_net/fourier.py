1a552b1a7673f0cb1d042549fd01f32832b817f9
gloomy <3126058535@qq.com>
2024-05-29 20:43:08 +0800

    提交代码

--- F_net/fourier.py
+++ F_net/fourier.py
@@ -1,4 +1,4 @@
-'''傅里叶变换'''
+"""Fourier Transforms used by FNet."""
 
 import functools
 
@@ -6,24 +6,38 @@ import jax
 from jax import lax
 import jax.numpy as jnp
 
+
 def two_dim_matmul(
-        x,
-        matrix_dim_one,
-        matric_dim_two,
-        precision = lax.Precision.DEFAULT):
-    '''2D矩阵乘法'''
-    return _two_dim_matmul(x, matrix_dim_one, matric_dim_two, precision)
+    x,
+    matrix_dim_one,
+    matrix_dim_two,
+    precision = lax.Precision.DEFAULT):
+  """Applies 2D matrix multiplication to 2D input arrays.
+
+  Args:
+    x: Input of shape [MAX_SEQ_LEN, HIDDEN_DIM]
+    matrix_dim_one: [MAX_SEQ_LEN, MAX_SEQ_LEN] matrix to apply to first
+      (sequence) dimension of input.
+    matrix_dim_two: [HIDDEN_DIM, HIDDEN_DIM] matrix to apply to second (hidden)
+      dimension of input.
+    precision: XLA precision for matrix multiplication operation.
+
+  Returns:
+    [MAX_SEQ_LEN, HIDDEN_DIM] array resulting from application of two
+      consecutive matrix multiplications.
+  """
+  return _two_dim_matmul(x, matrix_dim_one, matrix_dim_two, precision)
+
 
 @functools.partial(jax.jit, static_argnums=3)
-def _two_dim_matmul(
-                x,
-                matrix_dim_one,
-                matrix_dim_two,
-                precision):
-        return jnp.einsum(
-               "ij,jk,ni->nk",
-               x,
-               matrix_dim_one,
-               matrix_dim_two,
-               optimize=True,
-               precision=precision)
+def _two_dim_matmul(x, matrix_dim_one,
+                    matrix_dim_two,
+                    precision):
+  """Applies 2D matrix multiplication to 2D input arrays."""
+  return jnp.einsum(  # pytype: disable=wrong-arg-types  # jnp-type
+      "ij,jk,ni->nk",
+      x,
+      matrix_dim_two,
+      matrix_dim_one,
+      optimize=True,
+      precision=precision)
